f972020d8e94aad754dd691c3d4267e602fec067
fulaniteDeTal <iron.clad165@passmail.com>
2026-02-15 19:18:00 +0000

    D: added player and implimented basic rotation

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import *
 
 def main():
     print("Starting Asteroids with pygame version: 2.6.1")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    log_state()
-
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
     while True:
+        log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen) 
         pygame.display.flip()
+        dt = clock.tick(60)
+        
+        
 
 
 if __name__ == "__main__":
